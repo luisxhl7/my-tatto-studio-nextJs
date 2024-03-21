eb9e2e94308d62a337ee789ea99709c11680bed4
Luis carlos <luisxhl7@gmail.com>
2024-03-20 22:16:48 -0500

    adding page of tatuador

---
+++ src/app/tatuador/[id]/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { Banner } from "@/components/molecules/banner";
+import { tattooArtist } from "@/data/tattoo-artist";
+import Image from "next/image";
+import './tatuador-page.scss'
+
+interface TatuadorPageProps {
+    name: string;
+    photo: string;
+    url: string;
+    description: string;
+    portafolio: string[];
+    redesSociales: string[];
+    logros: string[];
+}
+
+interface PageParams {
+    id: string;
+}
+
+const Page = ({params}: {params: PageParams}) => {
+    
+    const [tatuador, setTatuador] = useState<TatuadorPageProps | null>(null);
+
+    useEffect(() => {
+        const filterTatuador = tattooArtist.find((tatuador) => tatuador.name === params.id);
+        if (filterTatuador !== undefined) {
+        setTatuador(filterTatuador);
+        } else {
+        setTatuador(null);
+        }
+    }, [params.id]);
+
+    return (
+        <div className="tatuadorPage">
+        <Banner imagesList={tatuador?.photo} title={tatuador?.name}/>
+
+        <p className="tatuadorPage__description">{tatuador?.description}</p>
+
+        <section className="tatuadorPage__content-images">
+            {tatuador?.portafolio.map((item, idx) => (
+            <img key={idx} src={item} alt="" className="tatuadorPage__images"/>
+            ))}
+        </section>
+        </div>
+    )
+}
+
+export default Page
