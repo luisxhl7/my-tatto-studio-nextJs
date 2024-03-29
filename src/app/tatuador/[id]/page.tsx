1466873425eeed57ae07ac1b31ad19941f67f635
Luis carlos <luisxhl7@gmail.com>
2024-03-28 20:23:01 -0500

    fix responseive in component of slider in section of designs

--- src/app/tatuador/[id]/page.tsx
+++ src/app/tatuador/[id]/page.tsx
@@ -32,6 +32,32 @@ const customSettings = {
   cssEase: "linear",
   arrows: false,
   swipeToSlide: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 const Page = ({ params }: { params: PageParams }) => {
