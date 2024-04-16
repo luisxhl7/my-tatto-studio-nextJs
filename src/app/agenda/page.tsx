d716f8b039df868a97b77fc77df3f3e51be0f9e1
Luis carlos <luisxhl7@gmail.com>
2024-04-16 15:33:09 -0500

    adding select ofartist in agenda for filter agenda

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -1,13 +1,25 @@
-'use client'
+"use client";
 
 import { Calendar, View } from "react-big-calendar";
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import { addHours, format } from 'date-fns';
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
 import { CardEvent } from "@/components/molecules/card-event/Card-event";
 import { getMessagesES, localizer } from "@/helpers";
-import { useState } from "react";
-import './agenda-page.scss'
+import React, { useEffect, useState } from "react";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import { Modal } from "@/components/atoms/modal/Modal";
+import "./agenda-page.scss";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  createTheme,
+} from "@mui/material";
+import Tatuador from "../tatuador/[id]/page";
+import { ThemeProvider } from "@emotion/react";
+import { useRouter } from "next/navigation";
 
 interface Params {
   id?: string;
@@ -19,129 +31,228 @@ interface DiaryPageProps {
 
 const myEventsList = [
   {
-    title: 'cumpleaños',
-    notes: 'cumpleaños de fulano',
-    dateInit: new Date(), 
-    dateEnd: addHours(new Date(), 2),
-    bgColor: '#fafafa',
-    user: {
-      _id: '123',
-      name: 'Fernando'
-    }
-  },
-  {
-    title: 'cumpleaños',
-    notes: 'cumpleaños de fulano',
+    title: "cumpleaños",
+    notes: "cumpleaños de fulano",
     dateInit: new Date(),
     dateEnd: addHours(new Date(), 2),
-    bgColor: '#fafafa',
+    bgColor: "#fafafa",
     user: {
-      _id: '123',
-      name: 'Fernando'
-    }
+      _id: "123",
+      name: "Fernando",
+    },
+  },
+  {
+    title: "cumpleaños",
+    notes: "cumpleaños de fulano",
+    dateInit: new Date(),
+    dateEnd: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Fernando",
+    },
   },
 ];
 
 const DiaryPage: React.FC<DiaryPageProps> = (props) => {
-  const [view, setView] = useState<View>('month');
+  const [view, setView] = useState<View>("month");
   const [date, setDate] = useState(new Date());
   const [openModal, setOpenModal] = useState(false);
 
-  // const { params } = props;
-  // if (params.id) {
-  //   console.log('consulta personalizada');
-  // } else {
-  //   console.log('consulta general');
-  // }
+  const [age, setAge] = React.useState(localStorage.getItem('tatuador') || "todos");
+  const { params } = props;
 
-  const onViewChanged = (event:any) => {
-    localStorage.setItem('lastView', event)
-    console.log(event);
-  }
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
 
-  const eventStyleGetter = (event:any) => {
-    
-    const isMyEvent = ('123' === event.user._id) || ('123' === event.user.uid)
+  const router = useRouter();
 
-    const style ={
-      backgroundColor: isMyEvent ? '#347CF7' : '#465660',
-      borderRadius: '0px',
-      opacity: 0.8,
-      color: 'chite'
+  useEffect(() => {
+    if (age !== "todos") {
+      localStorage.setItem('tatuador', age)
+      router.push(`/agenda/${age}`);
+    }else{
+      localStorage.setItem('tatuador', age)
+      router.push(`/agenda`);
     }
+    
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [age]);
+
+  useEffect(() => {
+    if (params.id) {
+      console.log(`consulta personalizada de ${params.id}`);
+    } else {
+      console.log('consulta general');
+    }
+  }, [params])
+  
+
+  const onViewChanged = (event: any) => {
+    localStorage.setItem("lastView", event);
+    console.log(event);
+  };
+
+  const eventStyleGetter = (event: any) => {
+    const isMyEvent = "123" === event.user._id || "123" === event.user.uid;
+
+    const style = {
+      backgroundColor: isMyEvent ? "#347CF7" : "#465660",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "chite",
+    };
 
     return {
-      style
-    }
-  }
+      style,
+    };
+  };
 
   const handlePrev = () => {
-    if (view === 'month') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
-    } else if (view === 'week') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth(), prevDate.getDate() - 7));
-    } else if (view === 'day') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth(), prevDate.getDate() - 1));
+    if (view === "month") {
+      setDate(
+        (prevDate) =>
+          new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1)
+      );
+    } else if (view === "week") {
+      setDate(
+        (prevDate) =>
+          new Date(
+            prevDate.getFullYear(),
+            prevDate.getMonth(),
+            prevDate.getDate() - 7
+          )
+      );
+    } else if (view === "day") {
+      setDate(
+        (prevDate) =>
+          new Date(
+            prevDate.getFullYear(),
+            prevDate.getMonth(),
+            prevDate.getDate() - 1
+          )
+      );
     }
   };
 
   const handleNext = () => {
-    if (view === 'month') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
-    } else if (view === 'week') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth(), prevDate.getDate() + 7));
-    } else if (view === 'day') {
-      setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth(), prevDate.getDate() + 1));
+    if (view === "month") {
+      setDate(
+        (prevDate) =>
+          new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1)
+      );
+    } else if (view === "week") {
+      setDate(
+        (prevDate) =>
+          new Date(
+            prevDate.getFullYear(),
+            prevDate.getMonth(),
+            prevDate.getDate() + 7
+          )
+      );
+    } else if (view === "day") {
+      setDate(
+        (prevDate) =>
+          new Date(
+            prevDate.getFullYear(),
+            prevDate.getMonth(),
+            prevDate.getDate() + 1
+          )
+      );
     }
   };
 
   const handleInitValue = () => {
     setDate(new Date());
-  }
+  };
 
   const handleNavigate = (newDate: Date) => {
     setDate(newDate);
   };
-  
+
   const handleOpenModal = () => {
-    setOpenModal(true)
-  }
+    setOpenModal(true);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
   return (
     <main className="agenda-page">
-      <div className="agenda-page__menu"> 
+      <div className="agenda-page__menu">
         <div className="agenda-page__menu__content-arrows">
-          <button onClick={handlePrev}>{'<'}</button>
+          <button
+            onClick={handlePrev}
+            className="agenda-page__menu__content-arrows__button-arrow"
+          >
+            <KeyboardArrowLeft />
+          </button>
           <button onClick={handleInitValue}>Hoy</button>
-          <button onClick={handleNext}>{'>'}</button>
+          <button
+            onClick={handleNext}
+            className="agenda-page__menu__content-arrows__button-arrow"
+          >
+            <KeyboardArrowRight />
+          </button>
         </div>
-        <button onClick={() => setView('month')} className="agenda-page__menu__btn">Mes</button>
-        <button onClick={() => setView('week')} className="agenda-page__menu__btn">Semana</button>
-        <button onClick={() => setView('day')} className="agenda-page__menu__btn">Día</button>
-
-        <label htmlFor="selectArtist">
-          Tatuador
-        </label>
-        <select name="" id="selectArtist">
-          <option value="Luis">Luis</option>
-          <option value="Keneth">Keneth</option>
-          <option value="Veronica">Veronica</option>
-          <option value="Yeison">Yeison</option>
-          <option value="Juan">Juan</option>
-        </select>
+        <div className="agenda-page__menu__content-date">
+          <button
+            onClick={() => setView("day")}
+            className="agenda-page__menu__content-date__btn"
+          >
+            Día
+          </button>
+          <button
+            onClick={() => setView("week")}
+            className="agenda-page__menu__content-date__btn"
+          >
+            Semana
+          </button>
+          <button
+            onClick={() => setView("month")}
+            className="agenda-page__menu__content-date__btn"
+          >
+            Mes
+          </button>
+        </div>
+        <div className="agenda-page__menu__content-selected">
+          <ThemeProvider theme={darkTheme}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Tatuador</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={"todos"}>Todos</MenuItem>
+                <MenuItem value={"keneth"}>Keneth</MenuItem>
+                <MenuItem value={"luis"}>Luis</MenuItem>
+                <MenuItem value={"veronica"}>Veronica</MenuItem>
+                <MenuItem value={"yeison"}>Yeison</MenuItem>
+                <MenuItem value={"juan"}>Juan</MenuItem>
+              </Select>
+            </FormControl>
+          </ThemeProvider>
+        </div>
       </div>
       <div className="agenda-page__content-agenda">
         <Calendar
-          culture='es'
+          culture="es"
           localizer={localizer}
           events={myEventsList}
           startAccessor="dateInit"
           endAccessor="dateEnd"
-          style={{ height: 'calc(100vh - 80px)'}}
-          messages={ getMessagesES() }
+          style={{ height: "calc(100vh - 80px)" }}
+          messages={getMessagesES()}
           eventPropGetter={eventStyleGetter}
           components={{
-            event: CardEvent
+            event: CardEvent,
           }}
           views={["month", "week", "day"]}
           view={view}
@@ -154,11 +265,10 @@ const DiaryPage: React.FC<DiaryPageProps> = (props) => {
       <button className="agenda-page__button-adding" onClick={handleOpenModal}>
         +
       </button>
-      {
-        openModal &&
+      {openModal && (
         <Modal>
           <div>
-            <form action="" >
+            <form action="">
               <h2>Cita</h2>
               <input type="text" />
               <input type="text" />
@@ -167,9 +277,9 @@ const DiaryPage: React.FC<DiaryPageProps> = (props) => {
             </form>
           </div>
         </Modal>
-      }
+      )}
     </main>
   );
-}
+};
 
 export default DiaryPage;
